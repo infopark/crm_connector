8c6b04e63899d21622032bfc8621525360181a49
Thomas Ritz <tritz@infopark.de>
2013-09-27 13:45:49 +0200

    script/console loads test crm connection

--- script/connector_initializer.rb
+++ script/connector_initializer.rb
@@ -1,9 +1,13 @@
 ### Connector ###
-require File.dirname(__FILE__) + '/../lib/infopark_crm_connector'
 
-### Connector configuration ###
+require "pathname"
+require File.dirname(__FILE__) + '/../lib/infopark_crm_connector'
+require File.dirname(__FILE__) + '/../test/local_config'
+
 Infopark::Crm.configure do |config|
-  config.url = 'http://localhost:4000'
-  config.login = 'root'
-  config.api_key = 'rootrootrootroot'
+  config.login = ENV['CRM_LOGIN'] || local_config['login']
+  config.api_key = ENV['CRM_API_KEY'] || local_config['api_key']
+  config.url = ENV['CRM_API_URL'] || local_config['url']
 end
+
+include Infopark::Crm
