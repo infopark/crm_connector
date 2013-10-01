39f5ff85be2a668596159ced10af360f4790a5c9
Thomas Ritz <tritz@infopark.de>
2013-10-01 14:27:01 +0200

    Revert "cruise in one bucket"
    This reverts commit 3cb36136de59a98741eea6af0866073a1cf6f2ab.

--- dcc_config.rb
+++ dcc_config.rb
@@ -1,9 +1,6 @@
 buckets :test do
-  # all in one bucket, because the tests use always the same crm tenant.
-  bucket(:unit_test).performs_rake_tasks(%w(
-    appraisal:install
-    appraisal:activeresource_3.2
-    appraisal:activeresource_3.1
-    appraisal:activeresource_3.0
-  ))
+  before_each_bucket.performs_rake_tasks('appraisal:install')
+  bucket(:unit_test_active_resource_3_0).performs_rake_tasks('appraisal:activeresource_3.0')
+  bucket(:unit_test_active_resource_3_1).performs_rake_tasks('appraisal:activeresource_3.1')
+  bucket(:unit_test_active_resource_3_2).performs_rake_tasks('appraisal:activeresource_3.2')
 end
