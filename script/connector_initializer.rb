bd9493f59a0998adc40264410f7075109614c998
Thomas Ritz <tritz@infopark.de>
2013-10-01 12:58:28 +0200

    travis fix

--- script/connector_initializer.rb
+++ script/connector_initializer.rb
@@ -5,9 +5,9 @@ require File.dirname(__FILE__) + '/../lib/infopark_crm_connector'
 require File.dirname(__FILE__) + '/../test/local_config'
 
 Infopark::Crm.configure do |config|
-  config.login = ENV['CRM_LOGIN'] || local_config['login']
-  config.api_key = ENV['CRM_API_KEY'] || local_config['api_key']
-  config.url = ENV['CRM_API_URL'] || local_config['url']
+  config.login = local_config['login']
+  config.api_key = local_config['api_key']
+  config.url = local_config['url']
 end
 
 include Infopark::Crm
